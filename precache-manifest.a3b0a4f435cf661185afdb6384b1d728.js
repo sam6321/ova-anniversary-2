d87e228153cd49943f79c50e31b833533dca1099
unknown <sam6321@live.com.au>
2019-12-15 20:37:43 +0800

    Updates

---
+++ precache-manifest.a3b0a4f435cf661185afdb6384b1d728.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9924ab293ddb1e830858a96ef2d6e0a6",
+    "url": "/ova-anniversary-2/index.html"
+  },
+  {
+    "revision": "2207ddf36949a49c88c6",
+    "url": "/ova-anniversary-2/static/css/main.a0598572.chunk.css"
+  },
+  {
+    "revision": "d31275b3f4ebc075261f",
+    "url": "/ova-anniversary-2/static/js/2.a114669a.chunk.js"
+  },
+  {
+    "revision": "2207ddf36949a49c88c6",
+    "url": "/ova-anniversary-2/static/js/main.22c572e2.chunk.js"
+  },
+  {
+    "revision": "49d80b3a9533a843e1a0",
+    "url": "/ova-anniversary-2/static/js/runtime-main.41894dba.js"
+  }
+]);
